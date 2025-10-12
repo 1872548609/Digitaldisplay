0b5ff89b6e34721f69103bdfe5786d3c6747425c
baocheng <1872548609@qq.com>
2025-10-12 18:57:04 +0800

    修改iot内按键逻辑

--- code/optical_fiber_osal/keil/Objects/apm32f0xx_ob.d
+++
@@ -1,8 +0,0 @@
-.\objects\apm32f0xx_ob.o: ..\code\components\driver\lib\apm32f0xx\src\apm32f0xx_ob.c
-.\objects\apm32f0xx_ob.o: ..\code\components\driver\lib\apm32f0xx\inc\apm32f0xx_ob.h
-.\objects\apm32f0xx_ob.o: ..\code\main\apm32f0xx.h
-.\objects\apm32f0xx_ob.o: ..\code\components\hal\target\apm32f051\CMSIS\core_cm0plus.h
-.\objects\apm32f0xx_ob.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\apm32f0xx_ob.o: ..\code\components\hal\target\apm32f051\CMSIS\core_cmInstr.h
-.\objects\apm32f0xx_ob.o: ..\code\components\hal\target\apm32f051\CMSIS\core_cmFunc.h
-.\objects\apm32f0xx_ob.o: ..\code\main\system_apm32f0xx.h
--- code/optical_fiber_osal/keil/Objects/system_apm32f0xx.d
+++
@@ -1,7 +0,0 @@
-.\objects\system_apm32f0xx.o: ..\code\components\hal\target\apm32f051\CMSIS\system_apm32f0xx.c
-.\objects\system_apm32f0xx.o: ..\code\main\apm32f0xx.h
-.\objects\system_apm32f0xx.o: ..\code\components\hal\target\apm32f051\CMSIS\core_cm0plus.h
-.\objects\system_apm32f0xx.o: D:\keil5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\system_apm32f0xx.o: ..\code\components\hal\target\apm32f051\CMSIS\core_cmInstr.h
-.\objects\system_apm32f0xx.o: ..\code\components\hal\target\apm32f051\CMSIS\core_cmFunc.h
-.\objects\system_apm32f0xx.o: ..\code\main\system_apm32f0xx.h
